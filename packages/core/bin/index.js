faa99f5ea73514930ecc0e2fff258161561d1270
franki <8024ldbryant@gmail.com>
2022-07-05 23:39:07 +0800

    util add add function

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
+const utils = require("@react-xx-cli/utils");
+
+utils();
+
+console.log(utils.add(1, 2));
+
 console.log('hello react-xx-cli');
--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -3,5 +3,11 @@
 module.exports = utils;
 
 function utils() {
-    // TODO 123
+    console.log('hello utils');
 }
+
+function add(a, b) {
+    return a + b;
+}
+
+module.exports.add = add;
