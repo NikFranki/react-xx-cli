fee8ab8dd836b33b0d63f2b188ae13c23f76ef0c
franki <8024ldbryant@gmail.com>
2022-07-17 15:45:48 +0800

    check user home

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
+module.exports = core;
+
 const semver = require('semver');
-const colors = require('colors');
+const colors = require('colors/safe');
+const userHome = require('user-home');
+const pathExists = require('path-exists').sync;
 
 // require 可以加载的文件类型是 .js/.json/.node 如果是其他后缀的文件，一律按照加载 js 文件的方式去解析，解析不了就报错
 // .js -> module.exports exports
@@ -10,23 +14,22 @@ const log = require('@react-xx-cli/log');
 const pkg = require('../package.json');
 const constant = require('./const');
 
-module.exports = core;
-
 function core() {
     try {
         checkPkgVersion();
         checkNodeVersion();
         checkRoot();
+        checkUserHome();
     } catch (error) {
         log.error(error.message);
     }
 }
 
-function checkPkgVersion(params) {
+function checkPkgVersion() {
     log.info('cli', pkg.version);
 }
 
-function checkNodeVersion(params) {
+function checkNodeVersion() {
     // 第一步检查当前版本
     const currentVersion = process.version;
     // 第二步检查最低版本
@@ -36,7 +39,13 @@ function checkNodeVersion(params) {
     }
 }
 
-function checkRoot(params) {
+function checkRoot() {
     const rootCheck = require('root-check');
     rootCheck();
+}
+
+function checkUserHome() {
+    if (!userHome || !pathExists(userHome)) {
+        throw new Error(`当前登录用户 ${colors.red(userHome)} 不存在`);
+    }
 }
